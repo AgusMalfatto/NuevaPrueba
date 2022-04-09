40ae817dd358b79dfc81ca2448ebf5622b01eac0
AgusMalfatto <agus.malfatto@hotmail.com>
2022-04-09 13:42:26 -0300

    Primer cambio

---
+++ Segunda_Prueba.py
@@ -0,0 +1,9 @@
+sexo = input("Ingrese su sexo: ")
+
+if sexo == "masc":
+    print("Usted es un machote")
+elif sexo == "fem":
+    print("Usted es una mujerzota")
+else:
+    print("No digas mamadas Mary Jane")
+
