3f45c880a1156450b79d001178dfefa2d0599277
AgusMalfatto <agus.malfatto@hotmail.com>
2022-04-10 10:17:34 -0300

    Teoría Python

--- Segunda_Prueba.py
+++ Segunda_Prueba.py
@@ -1,9 +1,38 @@
-numero = 20
+''' TIPOS DE DATOS '''
+
+''' Enteros '''
+
+numero = 10
 print(numero)
+''' La función 'Type' indica el tipo de dato que contiene la variable '''
+print(type(numero))
 
-if numero>0:
-    print("Correcto")
-else:
-    print("Incorrecto")
+''' Flotantes '''
 
-    
+flotante = 5.3
+print(flotante)
+print(type(flotante))
+
+''' Strings: Pueden ir con comillas simples o dobles '''
+
+cadena = 'a'
+nuevaCadena = "Hola"
+print(cadena, nuevaCadena)
+print(type(cadena))
+print(type(nuevaCadena))
+
+''' Booleanos: La primera letra del valor debe estar en mayúscula '''
+
+booleano = True
+
+print(booleano)
+print(type(booleano))
+
+''' CONCATENACIÓN '''
+
+num1 = 10
+num2 = 23.6
+resultado = (num1 + num2) * 22 / 16
+
+print("El resultado es: ", resultado)
+
