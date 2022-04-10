38e8d6746910e4c45a22bb66ccd8413997b5ccb0
AgusMalfatto <agus.malfatto@hotmail.com>
2022-04-10 10:29:13 -0300

    Nueva teoría

--- Segunda_Prueba.py
+++ Segunda_Prueba.py
@@ -1,19 +1,19 @@
 ''' TIPOS DE DATOS '''
 
-''' Enteros '''
+# Enteros
 
 numero = 10
 print(numero)
-''' La función 'Type' indica el tipo de dato que contiene la variable '''
+# La función 'Type' indica el tipo de dato que contiene la variable 
 print(type(numero))
 
-''' Flotantes '''
+# Flotantes 
 
 flotante = 5.3
 print(flotante)
 print(type(flotante))
 
-''' Strings: Pueden ir con comillas simples o dobles '''
+# Strings: Pueden ir con comillas simples o dobles 
 
 cadena = 'a'
 nuevaCadena = "Hola"
@@ -21,18 +21,38 @@ print(cadena, nuevaCadena)
 print(type(cadena))
 print(type(nuevaCadena))
 
-''' Booleanos: La primera letra del valor debe estar en mayúscula '''
+# Booleanos: La primera letra del valor debe estar en mayúscula 
 
 booleano = True
 
 print(booleano)
 print(type(booleano))
 
-''' CONCATENACIÓN '''
+# CONCATENACIÓN 
 
 num1 = 10
 num2 = 23.6
 resultado = (num1 + num2) * 22 / 16
 
 print("El resultado es: ", resultado)
+
+''' OPERADORES ARITMÉTICOS '''
+
+num1 = 8
+num2 = 6
+
+# División entera
+
+resultado = num1 // num2
+print(resultado)
+
+# Resto de una división
+
+resultado = num1 % num2
+print(resultado)
+
+# Potencia
+
+resultado = num1 ** num2
+print(resultado)
 
